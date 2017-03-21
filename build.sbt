09c4aac0e1143c264645425de62e2b7beac1d1b9
Daniel Shuy <daniel_shuy@hotmail.com>
2017-03-21 20:06:32 +0800

    Renamed project from sbt-scripted-test to sbt-scripted-scalatest

--- build.sbt
+++ build.sbt
@@ -2,13 +2,13 @@ sbtPlugin := true
 
 organization := "com.github.daniel-shuy"
 
-name := "sbt-scripted-test"
+name := "sbt-scripted-scalatest"
 
 version := "0.1.0-SNAPSHOT"
 
 licenses := Seq("Apache License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
 
-homepage := Some(url("https://github.com/daniel-shuy/sbt-scripted-test"))
+homepage := Some(url("https://github.com/daniel-shuy/scripted-scalatest-sbt-plugin"))
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.1"
---
+++ src/main/scala/com/github/daniel/shuy/sbt/scripted/scalatest/SbtScriptedScalaTest.scala
@@ -0,0 +1,39 @@
+package com.github.daniel.shuy.sbt.scripted.scalatest
+
+import org.scalatest.Suite
+import sbt.{AutoPlugin, PluginTrigger, Setting, SettingKey, TaskKey}
+
+object SbtScriptedScalaTest extends AutoPlugin {
+  override def trigger: PluginTrigger = allRequirements
+
+  sealed abstract class ScriptedTestStacks(val shortstacks: Boolean, val fullstacks: Boolean)
+  case object NoStacks extends ScriptedTestStacks(false, false)
+  case object ShortStacks extends ScriptedTestStacks(true, false)
+  case object FullStacks extends ScriptedTestStacks(true, true)
+
+  object autoImport {
+    lazy val scriptedScalaTestDurations: SettingKey[Boolean] = SettingKey("scripted-scalatest-durations", "If false, will not display durations of tests.")
+    lazy val scriptedScalaTestStacks: SettingKey[ScriptedTestStacks] = SettingKey("scripted-scalatest-stacks", "Length of stack traces to print.")
+    lazy val scriptedScalaTestStats: SettingKey[Boolean] = SettingKey("scripted-scalatest-stats", "If false, will not display various statistics of tests.")
+    lazy val scriptedScalaTestSpec: TaskKey[Suite] = TaskKey("scripted-scalatest-spec", "The ScalaTest Spec.")
+    lazy val scriptedScalaTest: TaskKey[Unit] = TaskKey("scripted-scalatest", "Executes all ScalaTest tests for SBT plugin.")
+  }
+  import autoImport._
+
+  override def projectSettings: Seq[Setting[_]] = Seq(
+    scriptedScalaTestDurations := true,
+    scriptedScalaTestStacks := NoStacks,
+    scriptedScalaTestStats := true,
+
+    scriptedScalaTest := {
+      val stacks = scriptedScalaTestStacks.value
+
+      scriptedScalaTestSpec.value.execute(
+        durations = scriptedScalaTestDurations.value,
+        shortstacks = stacks.shortstacks,
+        fullstacks = stacks.fullstacks,
+        stats = scriptedScalaTestStats.value
+      )
+    }
+  )
+}
